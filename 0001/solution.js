602839024da79788fbcf861c1bd739599ade2ff1
crown <chenjuncrown@gmail.com>
2018-09-13 22:24:52 +0800

    optimize($JS): 0001

--- 0001/solution.js
+++ 0001/solution.js
@@ -4,17 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const answer = []
-    const len = nums.length
-    nums.some((item, index) => {
-        for (let idx = index + 1; idx < len; idx++) {
-            console.log(item, nums[idx])
-            if (item + nums[idx] === target) {
-                answer.push(index, idx)
-                return true
-            }
-        }
-        return false
-    })
-    return answer
+  let indexTable = {}
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i]
+    if (indexTable[complement] !== undefined) {
+      return [indexTable[complement], i]
+    }
+    indexTable[nums[i]] = i
+  }
 }
