bf5dc6a4019763f09dcaa6131f525dd9cd0e1327
crown <chenjuncrown@gmail.com>
2018-12-10 22:15:14 +0800

    update(0002): Improve performance

--- algorithms/0002/main.go
+++ algorithms/0002/main.go
@@ -7,28 +7,27 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	resPre := &ListNode{}
-	cur := resPre
-	carry := 0
+	answer := &ListNode{}
+	current := answer
+	tmp := 0
 
-	for l1 != nil || l2 != nil || carry > 0 {
-		sum := carry
-
+	for l1 != nil || l2 != nil {
+		sum := tmp
 		if l1 != nil {
 			sum += l1.Val
 			l1 = l1.Next
 		}
-
 		if l2 != nil {
 			sum += l2.Val
 			l2 = l2.Next
 		}
-
-		carry = sum / 10
-
-		cur.Next = &ListNode{Val: sum % 10}
-		cur = cur.Next
+		tmp = sum / 10
+		current.Next = &ListNode{Val: sum % 10}
+		current = current.Next
+	}
+	if tmp != 0 {
+		current.Next = &ListNode{Val: tmp}
 	}
 
-	return resPre.Next
+	return answer.Next
 }
--- algorithms/0002/main.js
+++ algorithms/0002/main.js
@@ -11,29 +11,25 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-  if (l1 == null) {
-    return l2
-  }
-  if (l2 == null) {
-    return l1
-  }
-  let dummy = new ListNode()
-  let pt = dummy
-  let carry = 0
-  while (l1 != null || l2 != null || carry === 1) {
-    let sum = carry
-    if (l1 != null) {
-      sum += l1.val
+  const answer = new ListNode()
+  let copy = answer
+  let tmp = 0
+  while (l1 || l2) {
+    const a = l1 ? l1.val : 0
+    const b = l2 ? l2.val : 0
+    const sum = a + b + tmp
+    tmp = Math.floor(sum / 10)
+    copy.next = new ListNode(sum % 10)
+    copy = copy.next
+    if (l1) {
       l1 = l1.next
     }
-    if (l2 != null) {
-      sum += l2.val
+    if (l2) {
       l2 = l2.next
     }
-    let node = new ListNode(sum % 10)
-    pt.next = node
-    pt = pt.next
-    carry = sum > 9 ? 1 : 0
   }
-  return dummy.next
+  if (tmp) {
+    copy.next = new ListNode(tmp)
+  }
+  return answer.next
 }
